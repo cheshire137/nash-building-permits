0b85be857471cc2bbcfa3dad3b7c281d3be36a62
Sarah Vessels <cheshire137@gmail.com>
2019-04-13 11:45:21 -0500

    Start on map rendering

--- src/components/PermitMap.js
+++ src/components/PermitMap.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 class PermitMap extends Component {
   render() {
     const { data } = this.props;
+    const firstRow = data[0];
+    if (!firstRow) {
+      return null;
+    }
+    if (!firstRow.mapped_location || !firstRow.mapped_location.coordinates) {
+      return null;
+    }
+
+    const position = firstRow.mapped_location.coordinates;
+    console.log(position);
 
     return (
-      <div>
-        map {data.length}
+      <div className="map">
+        <Map
+          style={{height: '350px'}}
+          center={position}
+          className="map__reactleaflet"
+          zoom="13"
+        >
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
