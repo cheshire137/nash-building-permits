98a2f3ae8d855f346f5358bcfacd7e984aca4db9
Sarah Vessels <cheshire137@gmail.com>
2019-04-13 11:11:28 -0500

    Cache responses from API for a day

--- src/models/SocrataApi.js
+++ src/models/SocrataApi.js
@@ -1,18 +1,51 @@
+import LocalStorage from './LocalStorage';
+
+const cachedUrlsKey = 'cached-urls';
+const cachedResponsesKey = 'cached-responses';
+
+const daysBetween = function(d1, d2) {
+  const diff = Math.abs(d1.getTime() - d2.getTime());
+  return diff / (1000 * 60 * 60 * 24);
+};
+
 class SocrataApi {
   constructor(appToken, baseUrl) {
     this.appToken = appToken;
     this.baseUrl = baseUrl;
   }
 
-  get(query) {
-    const url = `${this.baseUrl}${query || ''}`;
-    return fetch(url, {
+  urlFor(query) {
+    return `${this.baseUrl}${query || ''}`;
+  }
+
+  async get(query) {
+    const url = this.urlFor(query);
+    const cachedUrls = LocalStorage.get(cachedUrlsKey) || {};
+    const cachedResponses = LocalStorage.get(cachedResponsesKey) || {};
+    const today = new Date();
+
+    if (cachedUrls[url]) {
+      const dateFetched = new Date(cachedUrls[url]);
+      if (daysBetween(today, dateFetched) <= 1) {
+        console.log('using cached data for', url);
+        return cachedResponses[url];
+      }
+    }
+
+    console.log('GET', url);
+    const response = await window.fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'X-App-Token': this.appToken
       }
-    });
+    })
+    const json = await response.json();
+    cachedResponses[url] = json;
+    cachedUrls[url] = today;
+    LocalStorage.set(cachedUrlsKey, cachedUrls);
+    LocalStorage.set(cachedResponsesKey, cachedResponses);
+    return json;
   }
 }
 
