efca93ccdc82f9bd2f4f53df6de530258efa30f7
Sarah Vessels <cheshire137@gmail.com>
2019-04-20 09:28:55 -0500

    Show more details in permit list

--- src/components/PermitList.js
+++ src/components/PermitList.js
@@ -11,13 +11,50 @@ class PermitList extends Component {
     }
 
     return (
-      <ul className="permit-list pt-2">
-        {geocodedPermits.map(permit => (
-          <li key={permit.key()}>
-            {permit.type}
-          </li>
-        ))}
-      </ul>
+      <div className="permit-list pt-2">
+        <table className="width-full text-center">
+          <thead>
+            <tr>
+              <th
+                className="pb-2"
+              >Address</th>
+              <th
+                className="pb-2 no-wrap"
+              >Zip code</th>
+              <th
+                className="pb-2"
+              >Type</th>
+              <th
+                className="pb-2 no-wrap"
+              >Sub-type</th>
+              <th
+                className="pb-2 no-wrap"
+              >Council district</th>
+            </tr>
+          </thead>
+          <tbody>
+            {geocodedPermits.map(permit => (
+              <tr key={permit.key()}>
+                <td
+                  className="border-top"
+                >{permit.address}</td>
+                <td
+                  className="border-top"
+                >{permit.zipCode}</td>
+                <td
+                  className="border-top no-wrap"
+                >{permit.type}</td>
+                <td
+                  className="border-top"
+                >{permit.subtype}</td>
+                <td
+                  className="border-top"
+                >{permit.councilDistrict}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
